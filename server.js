25eefddb7dadc9f7d6e0a0d868edbc968c117212
MahmoodIRAKE <mhmod.eraqi@gmail.com>
2022-02-13 06:53:46 +0200

    final upload

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // only when ready to deploy
-// app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use(cors(corsConfig));
 app.use(express.json())
 app.use(helmet())
@@ -47,9 +47,9 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 // only when ready to deploy
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
